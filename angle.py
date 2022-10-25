6ed2dd37e33a496653f9cc5aa560a718a82eb210
shivani429 <113819244+shivani429@users.noreply.github.com>
2022-10-25 11:32:13 +0530

    Add files via upload

---
+++ angle.py
@@ -0,0 +1,14 @@
+import turtle
+t=turtle.Turtle()
+for i in range(10,101,10):
+    t.circle(i)
+t.right(90)
+for i in range(10,101,10):
+    t.circle(i)
+t.right(90)
+for i in range(10,101,10):
+    t.circle(i)
+t.right(90)
+for i in range(10,101,10):
+    t.circle(i)
+t.right(90)
